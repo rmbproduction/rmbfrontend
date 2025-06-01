a4d70024b227db06a7dd45efcd23b749946aaf06
rmbproduction <repairmybike81@gmail.com>
2025-06-01 15:10:29 -0700

    .\vercel.json

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ScrollToTop from '../components/ScrollToTop';
-// ... other imports ...
+import Profile from '../pages/Profile';
+import Home from '../pages/Home';
+import VehicleList from '../pages/VehicleList';
+import SellVehicle from '../pages/SellVehicle';
+import NotFound from '../pages/NotFound';
+import ProtectedRoute from '../components/ProtectedRoute';
 
 const AppRoutes: React.FC = () => {
   return (
     <>
       <ScrollToTop />
       <Routes>
-        {/* ... existing routes ... */}
+        <Route path="/" element={<Home />} />
+        <Route path="/vehicles" element={<VehicleList />} />
+        <Route 
+          path="/profile" 
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/sell-vehicle" 
+          element={
+            <ProtectedRoute>
+              <SellVehicle />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
