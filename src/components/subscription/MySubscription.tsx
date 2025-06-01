00f5c892a28995f1f29644e5dd2b0d4e789b0870
rmbproduction <repairmybike81@gmail.com>
2025-06-01 15:37:56 -0700

    .\src\components\subscription\MySubscription.tsx

--- src/components/subscription/MySubscription.tsx
+++ src/components/subscription/MySubscription.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import SubscriptionOverview from './SubscriptionOverview';
-import ForSaleVehicles from '../ForSaleVehicles';
 
 const MySubscription: React.FC = () => {
   return (
     <div className="w-full">
       <SubscriptionOverview />
-      <div className="mt-8">
-        <ForSaleVehicles />
-      </div>
     </div>
   );
 };
