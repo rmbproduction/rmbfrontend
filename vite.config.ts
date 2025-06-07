752122d9d5a2374c03a65c4c4d2551c4c543b281
rmbproduction <repairmybike81@gmail.com>
2025-06-07 06:20:19 -0700

    .\vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on mode
-  const env = loadEnv(mode, process.cwd(), '');
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     base: '/',
